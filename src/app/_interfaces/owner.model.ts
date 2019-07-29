088bf9a8b590f4b8fb67be883e52a4070a179deb
hbtodev <humberto.s.santos@outlook.com>
2019-07-29 18:04:42 -0300

    Inclusão de page details

--- src/app/_interfaces/owner.model.ts
+++ src/app/_interfaces/owner.model.ts
@@ -1,6 +1,12 @@
+import { Account } from "./account.model";
+
+
 export interface Owner{
     id: string;
     name: string;
     dateOfBirth: Date;
     address: string;
+
+    accounts?: Account[];
+
 }
--- src/app/owner/owner-list/owner-list.component.ts
+++ src/app/owner/owner-list/owner-list.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Owner } from './../../_interfaces/owner.model';
+import { Router, ActivatedRoute } from '@angular/router';
 import { RepositoryService } from './../../shared/services/repository.service';
-import { Owner } from './../interfaces/owner.model';
 import { ErrorHandlerService } from './../../shared/services/error-handler.service';
- 
  
 @Component({
   selector: 'app-owner-list',
@@ -10,26 +10,30 @@ import { ErrorHandlerService } from './../../shared/services/error-handler.servi
   styleUrls: ['./owner-list.component.css']
 })
 export class OwnerListComponent implements OnInit {
-
   public owners: Owner[];
   public errorMessage: string = '';
 
-  constructor(private repository: RepositoryService, private errorHandler: ErrorHandlerService) { }
- 
+  constructor(private repository: RepositoryService, private errorHandler: ErrorHandlerService, private router: Router) { }
+
   ngOnInit() {
     this.getAllOwners();
   }
- 
-  public getAllOwners(){
 
+  public getAllOwners() {
     let apiAddress: string = "api/owner";
-    
     this.repository.getData(apiAddress)
-      .subscribe(res => { this.owners = res as Owner[];
+      .subscribe(res => {
+        this.owners = res as Owner[];
       },
-        (error) => {
-          this.errorHandler.handleError(error);
-          this.errorMessage = this.errorHandler.errorMessage;
-        })
+      (error) => {
+        this.errorHandler.handleError(error);
+        this.errorMessage = this.errorHandler.errorMessage;
+      })
   }
+
+  public getOwnerDetails(id){
+    let detailsUrl: string = `/owner/details/${id}`
+    this.router.navigate([detailsUrl]);
+  }
+
 }
